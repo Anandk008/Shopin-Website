b6da33d4c559433afc51ab7cfc051d03b8f2fbad
Anandk008 <Anandkadale1@gmail.com>
2023-03-19 14:46:19 +0530

    Homepage Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Categoty from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import NewsLetter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
+
 function App() {
-    return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Categoty />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <NewsLetter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
+  );
 }
 
 export default App;
