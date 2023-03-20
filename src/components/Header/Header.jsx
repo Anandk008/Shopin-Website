e4483692cd580430bded90f3882ab0b4db5e2e38
Anandk008 <Anandkadale1@gmail.com>
2023-03-20 23:02:48 +0530

    Search Fun Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,6 +12,7 @@ import Search from "./Search/Search";
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -37,7 +38,7 @@ const Header = () => {
           </ul>
           <div className="center">Shopy</div>
           <div className="right">
-            <TbSearch />
+            <TbSearch onClick={() => setShowSearch(true)}/>
             <AiOutlineHeart />
             <span className="cart-icon" onClick={() => setShowCart(true)}>
               <CgShoppingCart />
@@ -47,6 +48,7 @@ const Header = () => {
         </div>
       </header>
       {showCart && <Cart setShowCart={setShowCart} />}
+      {showSearch && <Search setShowSearch={setShowSearch } />} 
     </>
   );
 };
--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,6 +1,29 @@
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
+const Search = ({ setShowSearch }) => {
+  return (
+    <div className="search-model">
+      <div className="form-field">
+        <input type="text" autoFocus placeholder="Search For Products" />
+        <MdClose onClick={() => setShowSearch(false)} />
+      </div>
+      <div className="search-result-content">
+        <div className="search-result">
+          <div className="search-result-item">
+            <div className="img-container">
+              <img src={prod} alt="" />
+            </div>
+            <div className="prod-details">
+                    <span className="name">Product Name</span>
+                    <span className="desc">Product Desc</span>
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
