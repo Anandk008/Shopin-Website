b4d6cdcb711f752ef887f58cecf3095296b3cb49
Anandk008 <Anandkadale1@gmail.com>
2023-03-20 16:42:59 +0530

    Shopping Page added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,43 @@
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+import { MdClose } from "react-icons/md";
+// import { BsCartX } from "react-icons/bs";
+import CartItem from './CartItem/CartItem'
+ 
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shoping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose />
+            <span className="text">Close</span>
+          </span>
+        </div>
+
+        {/* <div className="empty-cart"></div>
+            <BsCartX />
+            <span>No Products in Cart</span>
+            <button className="return-cta">Return to shop</button>
+        */}
+        </div> 
+
+      <>
+        <CartItem />
+
+        <div className="cart-footer">
+            <div className="subtotal">
+                <div className="text">Subtotal</div>
+                <div className="text-total">&#8377;1234</div>
+            </div>
+            <div className="button">
+                <div className="checkout-cta">Checkout</div>
+            </div>
+        </div>
+      </>
+    </div>
+  );
 };
 
 export default Cart;
